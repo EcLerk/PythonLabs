8ebd12d45063192399cfb6ad947840e9d26a3630
EcLerk <lerakachanovskaya@gmail.com>
2023-02-23 00:16:50 +0300

    Implemented Task3

--- Lab1/calculations.py
+++ Lab1/calculations.py
@@ -9,3 +9,13 @@ def calculate(operand1, operand2, operator):
         return operand1 / operand2
 
     raise Exception("Incorrect input. There's no such operand")
+
+
+def find_even(my_list):
+    my_even_list = list()
+
+    for i in my_list:
+        if i % 2 == 0:
+            my_even_list.append(i)
+
+    print(my_even_list)
